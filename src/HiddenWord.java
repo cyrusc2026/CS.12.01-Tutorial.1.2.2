68851837dbd4adbff63e3a188bfb10e40140d622
cyrusc2026 <cyrusc2026@student.cis.edu.hk>
2024-08-22 07:56:17 +0800

    Commit 1

---
+++ src/HiddenWord.java
@@ -0,0 +1,32 @@
+public class HiddenWord {
+    private final String hiddenWord;
+    public HiddenWord(String hiddenWord){
+        this.hiddenWord = hiddenWord.toUpperCase();
+    }
+    public String getHint(String input) throws IllegalArgumentException {
+        if (!input.toUpperCase().equals(input)){
+            throw new IllegalArgumentException("Your guess ( " + input + " ) does not contain all uppercase letters. Your guess must contain all uppercase letters!");
+        }
+        if (input.length()!=hiddenWord.length()) {
+            throw new IllegalArgumentException ("Your guess ( " + input + " ) has " + input.length() + " characters. The hidden word has " + hiddenWord.length() + " characters. Your guess must be a word with " + hiddenWord.length() + " characters!");
+        }
+        StringBuilder answer = new StringBuilder();
+        for (int i = 0; i < input.length(); i++){
+            boolean isSame = false;
+            boolean haveRepeat = false;
+            for (int j = 0; j < hiddenWord.length(); j++){
+                if (hiddenWord.substring(j,j+1).equals(input.substring(i,i+1))) {
+                    if (i==j) isSame = true;
+                    else haveRepeat = true;
+                }
+            }
+            if (isSame) answer.append(hiddenWord.charAt(i));
+            if (haveRepeat && !isSame) answer.append("+");
+            else if (!isSame || !haveRepeat) answer.append("*");
+        }
+        return answer.toString();
+    }
+    public String getHiddenWord(){
+        return hiddenWord;
+    }
+}
